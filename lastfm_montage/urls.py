b39eb9b38725b0cccdc0f386ff5aae0635e93fa1
Guilherme S <guibs225@gmail.com>
2022-04-05 20:30:01 -0300

    feat: adiciona renderização de template

--- lastfm_montage/urls.py
+++ lastfm_montage/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('web.urls')),
     path('api/', include('api.urls')),
-    path('/', include('web.urls')),
+    path('admin/', admin.site.urls),
 ]
--- web/views.py
+++ web/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+def index(request: HttpRequest):
+    return render(request, 'index.html')
