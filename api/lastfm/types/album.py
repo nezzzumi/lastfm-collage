1f4fec4f89dcd93d8dad5b8eaad989a62a48a5fb
Guilherme S <guibs225@gmail.com>
2022-04-05 22:45:40 -0300

    feature: adiciona art no método as_dict

--- api/lastfm/types/album.py
+++ api/lastfm/types/album.py
@@ -5,9 +5,9 @@ from api.lastfm.types.image import Image
 
 
 class Album:
-    def __init__(self, artist: Artist, image: List[Image], mbid: str, url: str, playcount: str, name: str) -> None:
+    def __init__(self, artist: Artist, images: List[Image], mbid: str, url: str, playcount: str, name: str) -> None:
         self.artist = artist
-        self.images = image
+        self.images = images
         self.mbid = mbid
         self.url = url
         self.playcount = int(playcount)
@@ -16,5 +16,7 @@ class Album:
     def as_dict(self) -> dict:
         return {
             "artist": self.artist.name,
-            "name": self.name
+            "name": self.name,
+            # Último item = maior qualidade.
+            "art": self.images[-1].url
         }
