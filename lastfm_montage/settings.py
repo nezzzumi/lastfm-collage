55dd2c6fb80bba9a187ce4f402f341960891fae0
Guilherme S <guibs225@gmail.com>
2022-04-05 23:08:10 -0300

    feat: adiciona variável de ambiente `LASTFM_API_KEY`

--- lastfm_montage/settings.py
+++ lastfm_montage/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,6 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ns8jnv0uks1+y0%t9%wpgmuskdoq1x0+!ox5^x9+0m!eys3jbp'
+
+LASTFM_API_KEY = os.getenv('LASTFM_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
