5ac03924ab24f23cdd05897f64c8197f34c4b676
Guilherme S <guibs225@gmail.com>
2022-04-07 22:41:45 -0300

    revert: "refactor: renomeia `collage_background` para `collage`"
    This reverts commit b956aeaa34935bdec16058a931d6656c578112af.

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,15 +1,8 @@
-from django.conf import settings
 from django.http import HttpRequest, JsonResponse
-
-from lastfm import LastFM
 
 
 def generate(request: HttpRequest) -> JsonResponse:
-    last = LastFM(settings.LASTFM_API_KEY)
-    albums = last.get_top_albums('guibszin')
-
-    return JsonResponse({
-        "albums": [album.as_dict() for album in albums],
-    })
-
     # TODO
+    return JsonResponse({
+        "todo": True,
+    })
