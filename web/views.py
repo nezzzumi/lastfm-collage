b598034f5ec6839fab0a9d075e96edfeb426cfcf
Guilherme S <guibs225@gmail.com>
2022-04-07 22:27:10 -0300

    feat: adiciona método POST à index, e retorna colagem de álbuns mais escutados

--- web/views.py
+++ web/views.py
@@ -1,6 +1,26 @@
-from django.http import HttpRequest
+from django.conf import settings
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 
+from lastfm import LastFM
 
-def index(request: HttpRequest):
-    return render(request, 'index.html')
+
+def index(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        user = request.POST.get('user', '')
+
+        if not user:
+            # TODO: criar página de erro
+            return HttpResponseBadRequest('<h1>Parâmetros inválidos</h1>')
+
+        lastfm = LastFM(settings.LASTFM_API_KEY)
+
+        collage = lastfm.gen_top_albums_collage(user)
+        response = HttpResponse(content_type='image/png')
+        collage.save(response, 'PNG')
+
+        return response
+    elif request.method == 'GET':
+        return render(request, 'index.html')
+    else:
+        return HttpResponse('Método não permitido.', status=405)
