9ebb6d1812a7ea8ba82dae9182d4158f5802fe89
Guilherme S <guibs225@gmail.com>
2022-04-09 20:54:45 -0300

    feat: adiciona busca de imagem do artista através do site
    A API do last.fm não retorna a imagem dos artistas.

--- lastfm/types/artist.py
+++ lastfm/types/artist.py
@@ -1,8 +1,30 @@
+import io
+import re
+
+import requests
+from PIL import Image
+
+
 class Artist:
     def __init__(self, url: str, name: str, mbid: str) -> None:
         self.url = url
         self.name = name
         self.mbid = mbid
+        self.image = self._get_image()
+
+    def _get_image(self) -> Image.Image:
+        """
+        Busca imagem do artista no site do last.fm (a API não retorna imagem de artistas).
+        """
+
+        response = requests.get(self.url + '/+images')
+        found = re.search('https://lastfm.freetls.fastly.net/i/u/avatar170s/.*?(?=\")', response.text)
+
+        # Altera a URL para conseguir uma imagem com a resolução maior.
+        image_url = found.group().replace('avatar170s', '770x0')
+        image_content = requests.get(image_url).content
+
+        return Image.open(io.BytesIO(image_content))
 
     def as_dict(self) -> dict:
         return {
